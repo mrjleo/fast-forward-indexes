9895ea46fd240e567e55518a8e41e582e60dda70
mrjleo <mrjleo@users.noreply.github.com>
2024-03-07 14:15:51 +0100

    move config to pyproject.toml

--- fast_forward/__init__.py
+++ fast_forward/__init__.py
@@ -2,10 +2,12 @@
 .. include:: docs/main.md
 """
 
-__version__ = "0.1.0"
+import importlib.metadata
 
 from fast_forward.index import Mode
 from fast_forward.index.disk import OnDiskIndex
 from fast_forward.index.memory import InMemoryIndex
 from fast_forward.indexer import Indexer
 from fast_forward.ranking import Ranking
+
+__version__ = __version__ = importlib.metadata.version("fast-forward-indexes")
