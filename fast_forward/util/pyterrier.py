f0c0784ea40d3003ea18c6717697b8bc9efaa5da
mrjleo <mrjleo@users.noreply.github.com>
2024-03-09 21:42:52 +0100

    add PyTerrier transformers for scoring and interpolation

---
+++ fast_forward/util/pyterrier.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import pyterrier as pt
+
+from fast_forward.index import Index
+from fast_forward.ranking import Ranking
+
+
+class FFScore(pt.Transformer):
+    """PyTerrier transformer that computes scores using a Fast-Forward index."""
+
+    def __init__(self, index: Index) -> None:
+        """Constructor.
+
+        Args:
+            index (Index): The Fast-Forward index.
+        """
+        self._index = index
+        super().__init__()
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Compute the scores for all query-document pairs in the data frame.
+        The previous scores are moved to the "score_0" column.
+
+        Args:
+            df (pd.DataFrame): The PyTerrier data frame.
+
+        Returns:
+            pd.DataFrame: A new data frame with the computed scores.
+        """
+        ff_scores = self._index(
+            Ranking(
+                df.rename(columns={"qid": "q_id", "docno": "id"}),
+                copy=False,
+                is_sorted=True,
+            )
+        )._df.rename(columns={"q_id": "qid", "id": "docno"})
+
+        return df[["qid", "docno", "score"]].merge(
+            ff_scores[["qid", "docno", "score", "query"]],
+            on=["qid", "docno"],
+            suffixes=["_0", None],
+        )
+
+
+class FFInterpolate(pt.Transformer):
+    """PyTerrier transformer that interpolates scores computed by `FFScore`."""
+
+    def __init__(self, alpha: float) -> None:
+        """Constructor.
+
+        Args:
+            alpha (float): The interpolation parameter.
+        """
+        # attribute name needs to be exactly this for pyterrier.GridScan to work
+        self.alpha = alpha
+        super().__init__()
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Interpolate the scores for all query-document pairs in the data frame as alpha * score_0 + (1 - alpha) * score.
+
+        Args:
+            df (pd.DataFrame): The PyTerrier data frame.
+
+        Returns:
+            pd.DataFrame: A new data frame with the interpolated scores.
+        """
+        new_df = df[["qid", "docno"]].copy()
+        new_df["score"] = self.alpha * df["score_0"] + (1 - self.alpha) * df["score"]
+        return new_df
