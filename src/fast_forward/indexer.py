b3415161bbb40c5ffaa09490b1f7709f0e494d5d
mrjleo <mrjleo@users.noreply.github.com>
2024-12-06 16:56:56 +0100

    update docstrings for indexer.py

--- src/fast_forward/indexer.py
+++ src/fast_forward/indexer.py
@@ -32,17 +32,14 @@ class Indexer(object):
         Optionally, a quantizer can be automatically be fit on the first batch(es) to be indexed. This requires the index to be empty.
         If a quantizer is provided, the first batch(es) will be buffered and used to fit the quantizer.
 
-        Args:
-            index (Index): The target index.
-            encoder (Encoder, optional): Document/passage encoder. Defaults to None.
-            encoder_batch_size (int, optional): Encoder batch size. Defaults to 128.
-            batch_size (int, optional): How many vectors to add to the index at once. Defaults to 2**16.
-            quantizer (Quantizer, optional): A quantizer to be fit and attached to the index. Defaults to None.
-            quantizer_fit_batches (int, optional): How many of the first batches to use to fit the quantizer. Defaults to 1.
-
-        Raises:
-            ValueError: When a quantizer is provided that has already been fit.
-            ValueError: When a quantizer is provided and the index already has vectors.
+        :param index: The target index.
+        :param encoder: Document/passage encoder.
+        :param encoder_batch_size: Encoder batch size.
+        :param batch_size: How many vectors to add to the index at once.
+        :param quantizer: A quantizer to be fit and attached to the index.
+        :param quantizer_fit_batches: How many of the first batches to use to fit the quantizer.
+        :raises ValueError: When a quantizer is provided that has already been fit.
+        :raises ValueError: When a quantizer is provided and the index already has vectors.
         """
         self._index = index
         self._encoder = encoder
@@ -76,13 +73,13 @@ class Indexer(object):
     ) -> None:
         """Add a batch to the index.
 
-        If this indexer has a quantizer to be fit, the inputs will be buffered until the desired amount of data for fitting
-        has been obtained. Afterwards, the quantizer is fit and attached to the index, and all buffered inputs are added at once.
+        If this indexer has a quantizer to be fit, the inputs will be buffered until the desired amount of data for fitting has been obtained.
+        Afterwards, the quantizer is fit and attached to the index, and all buffered
+        inputs are added at once.
 
-        Args:
-            vectors (np.ndarray): The vectors.
-            doc_ids (IDSequence, optional): Corresponding document IDs. Defaults to None.
-            psg_ids (IDSequence, optional): Corresponding passage IDs. Defaults to None.
+        :param vectors: The vectors.
+        :param doc_ids: Corresponding document IDs.
+        :param psg_ids: Corresponding passage IDs.
         """
         if self._quantizer is None:
             self._index.add(vectors, doc_ids, psg_ids)
@@ -124,14 +121,9 @@ class Indexer(object):
     def _encode(self, texts: Sequence[str]) -> np.ndarray:
         """Encode a list of strings (respecting the encoder batch size).
 
-        Args:
-            texts (Sequence[str]): The pieces of text to encode.
-
-        Returns:
-            np.ndarray: The vector representations.
-
-        Raises:
-            RuntimeError: When no encoder exists.
+        :param texts: The pieces of text to encode.
+        :raises RuntimeError: When no encoder exists.
+        :return: The vector representations.
         """
         if self._encoder is None:
             raise RuntimeError("An encoder is required.")
@@ -144,11 +136,9 @@ class Indexer(object):
 
     def from_dicts(self, data: Iterable[dict[str, str]]) -> None:
         """Index data from dictionaries.
-
         The dictionaries should have the key "text" and at least one of "doc_id" and "psg_id".
 
-        Args:
-            data (Iterable[Dict[str, str]]): An iterable of the dictionaries.
+        :param data: An iterable of the dictionaries.
         """
         texts, doc_ids, psg_ids = [], [], []
         for d in tqdm(data):
@@ -165,11 +155,9 @@ class Indexer(object):
 
     def from_index(self, index: Index) -> None:
         """Transfer vectors and IDs from another index.
-
         If the source index uses quantized representations, the vectors are reconstructed first.
 
-        Args:
-            index (Index): The source index.
+        :param index: The source index.
         """
         for vectors, doc_ids, psg_ids in tqdm(index.batch_iter(self._batch_size)):
             self._index_batch(vectors, doc_ids, psg_ids)
