762ad17bdc0aa6cc7b49d1e24e8a2279a5e20c07
mrjleo <mrjleo@users.noreply.github.com>
2024-12-06 16:51:09 +0100

    update docstrings for ranking.py

--- src/fast_forward/ranking.py
+++ src/fast_forward/ranking.py
@@ -17,15 +17,10 @@ LOGGER = logging.getLogger(__name__)
 def _attach_queries(df: pd.DataFrame, queries: Mapping[str, str]) -> pd.DataFrame:
     """Attach queries to a data frame.
 
-    Args:
-        df (pd.DataFrame): The data frame (same format as used by rankings).
-        queries (Dict[str, str]): Query IDs mapped to queries.
-
-    Raises:
-        ValueError: When the queries are incomplete.
-
-    Returns:
-        pd.DataFrame: The data frame with queries attached.
+    :param df: The data frame (same format as used by rankings).
+    :param queries: Query IDs mapped to queries.
+    :raises ValueError: When the queries are incomplete.
+    :return: The data frame with queries attached.
     """
     if not set(pd.unique(df["q_id"])).issubset(set(queries.keys())):
         raise ValueError("Queries are incomplete.")
@@ -38,11 +33,8 @@ def _add_ranks(df: pd.DataFrame) -> pd.DataFrame:
     """Add a new column ("rank") to a data frame that contains ranks of documents w.r.t. the queries
     (based on the document scores). Ranks start from `1`.
 
-    Args:
-        df (pd.DataFrame): The data frame to add the column to.
-
-    Returns:
-        pd.DataFrame: The data frame with the new column added.
+    :param df: The data frame to add the column to.
+    :return: The data frame with the new column added.
     """
     df_ranks = df.groupby("q_id").cumcount().to_frame() + 1
     df_ranks.columns = ("rank",)
@@ -51,14 +43,10 @@ def _add_ranks(df: pd.DataFrame) -> pd.DataFrame:
 
 def _minmax_normalize_scores(df: pd.DataFrame) -> pd.DataFrame:
     """Return a copy of a data frame with normalized scores (min-max).
-
     If all scores are equal, they are set to `0`.
 
-    Args:
-        df (pd.DataFrame): The input data frame.
-
-    Returns:
-        pd.DataFrame: A copy of the data frame with normalized scores.
+    :param df: The input data frame.
+    :return: A copy of the data frame with normalized scores.
     """
     new_df = df.copy()
     min_val = new_df["score"].min()
@@ -85,16 +73,13 @@ class Ranking(object):
     ) -> None:
         """Create a ranking from an existing data frame. Removes rows with NaN scores.
 
-        Args:
-            df (pd.DataFrame): Data frame containing IDs and scores.
-            name (str, optional): Method name. Defaults to None. Defaults to None.
-            queries (Dict[str, str], optional): Query IDs mapped to queries. Defaults to None.
-            dtype (np.dtype, optional): How the scores should be represented in the data frame. Defaults to np.float32.
-            copy (bool, optional): Whether to copy the data frame. Defaults to True.
-            is_sorted (bool, optional): Whether the data frame is already sorted (by score). Defaults to False.
-
-        Raises:
-            ValueError: When the queries are incomplete.
+        :param df: Data frame containing IDs and scores.
+        :param name: Method name.
+        :param queries: Query IDs mapped to queries.
+        :param dtype: How the scores should be represented in the data frame.
+        :param copy:  Whether to copy the data frame.
+        :param is_sorted: Whether the data frame is already sorted (by score).
+        :raises ValueError: When the queries are incomplete.
         """
         super().__init__()
         self.name = name
@@ -126,8 +111,7 @@ class Ranking(object):
     def has_queries(self) -> bool:
         """Whether the ranking has queries attached.
 
-        Returns:
-            bool: Whether queries exist.
+        :return: Whether queries exist.
         """
         return "query" in self._df.columns
 
@@ -135,57 +119,45 @@ class Ranking(object):
     def q_ids(self) -> set[str]:
         """The set of (unique) query IDs in this ranking. Only queries with at least one scored document are considered.
 
-        Returns:
-            Set[str]: The query IDs.
+        :return: The query IDs.
         """
         return self._q_ids
 
     def __getitem__(self, q_id: str) -> dict[str, float]:
         """Return the ranking for a query.
 
-        Args:
-            q_id (str): The query ID.
-
-        Returns:
-            Dict[str, float]: Document/passage IDs mapped to scores.
+        :param q_id: The query ID.
+        :return: Document/passage IDs mapped to scores.
         """
         return dict(self._df[self._df["q_id"] == q_id][["id", "score"]].values)
 
     def __len__(self) -> int:
         """Return the number of queries.
 
-        Returns:
-            int: The number of queries.
+        :return: The number of queries.
         """
         return len(self._q_ids)
 
     def __iter__(self) -> Iterator[str]:
         """Yield all query IDs.
 
-        Yields:
-            str: The query IDs.
+        :yield: A query ID.
         """
         yield from self._q_ids
 
     def __contains__(self, key: object) -> bool:
-        """Check whether a query ID is in the ranking.
+        """Check whether a query ID has associated document/passage IDs.
 
-        Args:
-            key (object): The query ID.
-
-        Returns:
-            bool: Whether the query ID has associated document/passage IDs.
+        :param key: The query ID.
+        :return: Whether the query ID is in the ranking.
         """
         return key in self._q_ids
 
     def __eq__(self, o: object) -> bool:
         """Check if this ranking is identical to another one. Only takes IDs and scores into account.
 
-        Args:
-            o (object): The other ranking.
-
-        Returns:
-            bool: Whether the two rankings are identical.
+        :param o: The other ranking.
+        :return: Whether the two rankings are identical.
         """
         if not isinstance(o, Ranking):
             return False
@@ -200,11 +172,8 @@ class Ranking(object):
         """Add either a constant or the corresponding scores of another ranking to this ranking's scores.
         Missing scores in either ranking are treated as zero.
 
-        Args:
-            o (Union[Ranking, int, float]): A ranking or a constant.
-
-        Returns:
-            Ranking: The resulting ranking with added scores.
+        :param o: A ranking or a constant.
+        :return: The resulting ranking with added scores.
         """
         if isinstance(o, Ranking):
             new_df = self._df.merge(
@@ -232,11 +201,8 @@ class Ranking(object):
     def __mul__(self, o: float) -> "Ranking":
         """Multiply this ranking's scores by a constant.
 
-        Args:
-            o (Union[int, float]): A constant.
-
-        Returns:
-            Ranking: The resulting ranking with multiplied scores.
+        :param o: A constant.
+        :return: The resulting ranking with multiplied scores.
         """
         if not isinstance(o, (int, float)):
             return NotImplemented
@@ -257,22 +223,16 @@ class Ranking(object):
     def __repr__(self) -> str:
         """Return a string representation of this ranking.
 
-        Returns:
-            str: The string representation.
+        :return: The string representation.
         """
         return self._df.__repr__()
 
     def attach_queries(self, queries: Mapping[str, str]) -> "Ranking":
         """Attach queries to the ranking.
 
-        Args:
-            queries (Dict[str, str]): Query IDs mapped to queries.
-
-        Raises:
-            ValueError: When the queries are incomplete.
-
-        Returns:
-            Ranking: The ranking with queries attached.
+        :param queries: Query IDs mapped to queries.
+        :raises ValueError: When the queries are incomplete.
+        :return: The ranking with queries attached.
         """
         return Ranking(
             self._df,
@@ -285,11 +245,9 @@ class Ranking(object):
 
     def normalize(self) -> "Ranking":
         """Normalize the scores (min-max) to be in `[0, 1]`.
-
         If all scores are equal, they are set to `0`.
 
-        Returns:
-            Ranking: The ranking with normalized scores.
+        :return: A ranking with normalized scores.
         """
         return Ranking(
             _minmax_normalize_scores(self._df),
@@ -302,11 +260,8 @@ class Ranking(object):
     def cut(self, cutoff: int) -> "Ranking":
         """For each query, remove all but the top-`k` scoring documents/passages.
 
-        Args:
-            cutoff (int): Number of best scores per query to keep (`k`).
-
-        Returns:
-            Ranking: The resulting ranking.
+        :param cutoff: Number of best scores per query to keep (`k`).
+        :return: The resulting ranking.
         """
         return Ranking(
             self._df.groupby("q_id").head(cutoff).reset_index(drop=True),
@@ -325,13 +280,10 @@ class Ranking(object):
         """Interpolate as `score = self.score * alpha + other.score * (1 - alpha)`.
         Missing scores in either ranking are treated as zero.
 
-        Args:
-            other (Ranking): Ranking to interpolate with.
-            alpha (float): Interpolation parameter.
-            normalize (bool): Perform min-max normalization. Defaults to False.
-
-        Returns:
-            Ranking: The resulting ranking.
+        :param other: Ranking to interpolate with.
+        :param alpha: Interpolation parameter.
+        :param normalize: Perform min-max normalization.
+        :return: The resulting ranking.
         """
         df1 = self._df if not normalize else _minmax_normalize_scores(self._df)
         df2 = other._df if not normalize else _minmax_normalize_scores(other._df)
@@ -355,14 +307,10 @@ class Ranking(object):
 
     def rr_scores(self, k: int = 60) -> "Ranking":
         """Re-score documents/passages using reciprocal rank (as used by [RRF](https://dl.acm.org/doi/10.1145/1571941.1572114)).
-
         A score is computed as `1 / (rank + k)`.
 
-        Args:
-            k (int): RR scoring parameter. Defaults to 60.
-
-        Returns:
-            Ranking: A new ranking with RR scores.
+        :param k: RR scoring parameter.
+        :return: A  ranking with RR scores.
         """
         new_df = _add_ranks(self._df)
         new_df["score"] = 1 / (new_df["rank"] + k)
@@ -380,13 +328,11 @@ class Ranking(object):
     ) -> None:
         """Save the ranking in a TREC runfile.
 
-        Args:
-            target (Path): Output file.
+        :param target: Output file.
         """
         df_out = _add_ranks(self._df)
         df_out["name"] = str(self.name)
         df_out["q0"] = "Q0"
-
         target.parent.mkdir(parents=True, exist_ok=True)
         df_out.to_csv(
             target,
@@ -406,14 +352,11 @@ class Ranking(object):
     ) -> "Ranking":
         """Create a Ranking object from a TREC run.
 
-        Args:
-            run (Run): TREC run.
-            name (str, optional): Method name. Defaults to None. Defaults to None.
-            queries (Dict[str, str], optional): Query IDs mapped to queries. Defaults to None.
-            dtype (np.dtype, optional): How the score should be represented in the data frame. Defaults to np.float32.
-
-        Returns:
-            Ranking: The resulting ranking.
+        :param run: TREC run.
+        :param name: Method name. Defaults to None.
+        :param queries: Query IDs mapped to queries.
+        :param dtype: How the score should be represented in the data frame.
+        :return: The resulting ranking.
         """
         df = pd.DataFrame.from_dict(dict(run)).stack().reset_index()
         df.columns = ("id", "q_id", "score")
@@ -428,13 +371,10 @@ class Ranking(object):
     ) -> "Ranking":
         """Create a Ranking object from a runfile in TREC format.
 
-        Args:
-            f (Path): TREC runfile to read.
-            queries (Dict[str, str], optional): Query IDs mapped to queries. Defaults to None.
-            dtype (np.dtype, optional): How the score should be represented in the data frame. Defaults to np.float32.
-
-        Returns:
-            Ranking: The resulting ranking.
+        :param f: TREC runfile to read.
+        :param queries: Query IDs mapped to queries.
+        :param dtype: How the score should be represented in the data frame.
+        :return: The resulting ranking.
         """
         df = pd.read_csv(
             f,
