7090137a01bb6a0d5ef208736c770deb039aea6e
mrjleo <mrjleo@users.noreply.github.com>
2024-12-13 10:52:41 +0100

    update top-level imports, set __all__

--- src/fast_forward/__init__.py
+++ src/fast_forward/__init__.py
@@ -2,14 +2,8 @@
 
 import importlib.metadata
 
-# in this specific case, the redundant aliases are recommended by pyright
-# ruff: noqa: PLC0414
-from fast_forward.index import Mode as Mode
-from fast_forward.index.disk import OnDiskIndex as OnDiskIndex
-from fast_forward.index.memory import InMemoryIndex as InMemoryIndex
-from fast_forward.quantizer.nanopq import NanoOPQ as NanoOPQ
-from fast_forward.quantizer.nanopq import NanoPQ as NanoPQ
-from fast_forward.ranking import Ranking as Ranking
-from fast_forward.util.indexer import Indexer as Indexer
+from fast_forward import encoder, index, quantizer, util
+from fast_forward.ranking import Ranking
 
+__all__ = ["encoder", "index", "quantizer", "util", "Ranking"]
 __version__ = importlib.metadata.version("fast-forward-indexes")
